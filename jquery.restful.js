30d3691b7377ebb0e321fabb8d3e55cb2c324053
Walker de Alencar Oliveira <walkeralencar@gmail.com>
2012-12-19 17:10:57 -0200

    Update jquery.restful.js

--- jquery.restful.js
+++ jquery.restful.js
@@ -1,11 +1,11 @@
 /*!
  * jQuery RESTful plugin
- * @author: @walkeralencar
+ * @author @walkeralencar
  * Licensed under the MIT license
  */
 (function ($) {
     "use strict";
-    var restful = 'RESTful',
+    var pluginName = 'jQuery RESTful Plugin',
         defaults = {
             dataType: 'json',
             accepts: 'application/json',
@@ -14,26 +14,26 @@
                 Accept: 'application/json'
             }
         },
-        actions = {
-            create: 'POST',
-            read: 'GET',
-            update: 'PUT',
-            destroy: 'DELETE'
+        action = {
+            post: 'POST',
+            get: 'GET',
+            put: 'PUT',
+            delete: 'DELETE'
         },
         RESTful = function () {
             this.request = function (options) {
                 // $.extend(true,...) is a recursive extend;
                 this.options = $.extend(true, {}, defaults, options);
                 this.defaults = defaults;
-                this.name = restful;
-                this.action = actions;
+                this.name = pluginName;
+                this.action = action;
 
                 switch (options.type) {
-                case this.action.read:
-                case this.action.create:
+                case this.action.get:
+                case this.action.post:
                     break;
-                case this.action.update:
-                case this.action.destroy:
+                case this.action.put:
+                case this.action.delete:
                     var xHttpMethodOverride = {
                         headers: {
                             'X-HTTP-Method-Override': options.type
@@ -47,32 +47,31 @@
 
             this.post = function (options) {
                 return this.request(
-                    $.extend(true, {}, options, { type: actions.create })
+                    $.extend(true, {}, options, { type: action.post })
                 );
             };
 
             this.get = function (options) {
                 return this.request(
-                    $.extend(true, {}, options, { type: actions.read })
+                    $.extend(true, {}, options, { type: action.get })
                 );
             };
 
             this.put = function (options) {
                 return this.request(
-                    $.extend(true, {}, options, { type: actions.update })
+                    $.extend(true, {}, options, { type: action.put })
                 );
             };
 
             this.delete = function (options) {
                 return this.request(
-                    $.extend(true, {}, options, { type: actions.destroy })
+                    $.extend(true, {}, options, { type: action.delete })
                 );
             };
 
             this.create = this.post;
             this.read = this.get;
             this.update = this.put;
-            this.destroy = this.delete;
         };
     $.restful = new RESTful();
 })(jQuery);
